6cadb14f3e7baf6f7473ce90fb312f60d6c3f084
Bl00k25 <koronosensei51@gmail.com>
2023-02-27 18:27:43 +0300

    Examen S3 DW&Apis

---
+++ src/Entity/Administrator.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\AdministratorRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=AdministratorRepository::class)
+ */
+class Administrator
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+}
---
+++ src/Entity/Teacher.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TeacherRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=TeacherRepository::class)
+ */
+class Teacher
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Name;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $IsUnderContract;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Subject;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->Name;
+    }
+
+    public function setName(string $Name): self
+    {
+        $this->Name = $Name;
+
+        return $this;
+    }
+
+    public function isIsUnderContract(): ?bool
+    {
+        return $this->IsUnderContract;
+    }
+
+    public function setIsUnderContract(bool $IsUnderContract): self
+    {
+        $this->IsUnderContract = $IsUnderContract;
+
+        return $this;
+    }
+
+    public function getSubject(): ?string
+    {
+        return $this->Subject;
+    }
+
+    public function setSubject(string $Subject): self
+    {
+        $this->Subject = $Subject;
+
+        return $this;
+    }
+}
